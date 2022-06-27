e1b22c59ec93362ebdae5c3ddb22ff960dd31d7b
oldremez <83083413+oldremez@users.noreply.github.com>
2022-06-28 00:54:47 +0300

    Test scenario a bit modified to be more… test

--- tests/test_positive.sh
+++ tests/test_positive.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 test_addr_1=$(gaiad keys show test_key_1 --keyring-backend test --output json | jq -r ".address")
-test_addr_2=$(gaiad keys show test_key_2 --keyring-backend test --output json | jq -r ".address")
 multisig_addr_1=$(gaiad keys show multisig_test --keyring-backend test --output json | jq -r ".address")
+denom="uatom"
+
+prev_balance=$(gaiad query bank balances "$test_addr_1" --output json  \
+    | jq -r ".balances[] | select(.denom == \"$denom\") | .amount")
 
 gaiad tx bank send \
     "$multisig_addr_1" \
     "$test_addr_1" \
-    1uatom \
+    "1$denom" \
     --gas=200000 \
-    --fees=1uatom \
+    --fees="1$denom" \
     --chain-id=testhub \
     --generate-only > unsignedTx.json
 
@@ -22,4 +25,11 @@ multisig broadcast cosmos test
 
 sleep 7
 
-gaiad query bank balances "$test_addr_1"
+new_balance=$(gaiad query bank balances "$test_addr_1" --output json  \
+    | jq -r ".balances[] | select(.denom == \"$denom\") | .amount")
+
+if (( new_balance > prev_balance )); then
+    echo "Success"
+else
+    echo "Fail"
+fi
