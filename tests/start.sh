8e077d2c5e81c4e1562519e3eb0897687e11d39d
oldremez <83083413+oldremez@users.noreply.github.com>
2022-06-27 15:02:19 +0300

    Better way to wait till gaia is started

--- tests/start.sh
+++ tests/start.sh
@@ -3,8 +3,18 @@
 gaiad start --minimum-gas-prices 0atom --mode validator &> gaia.log &
 minio server /data --console-address ":9001" > minio.log &
 
-sleep 5
-
 /bin/bash configure_minio.sh > minio_conf.log
+
+for i in {1..10}
+do
+    latest_block_hash=$(gaiad status 2>&1 | jq -r ".SyncInfo.latest_block_hash")
+    if [ "$latest_block_hash" != "" ]
+    then
+        echo "Gaia is started"
+        break
+    fi
+    echo "Gaia is syncing"
+    sleep 1
+done
 
 /bin/bash test_positive.sh
