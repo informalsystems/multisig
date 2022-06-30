877bacc4357774502abaee9dcdeb7b31db8166c4
oldremez <83083413+oldremez@users.noreply.github.com>
2022-06-30 11:27:06 +0300

    Test script refactored a bit

--- tests/test_positive.sh
+++ tests/test_positive.sh
@@ -24,9 +24,13 @@ wait_till_next_block() {
     done
 }
 
+get_balance(){
+    gaiad query bank balances "$1" --output json  \
+        | jq -r ".balances[] | select(.denom == \"$denom\") | .amount"
+}
+
 @test "Positive scenario" {
-    prev_balance=$(gaiad query bank balances "$test_addr_1" --output json  \
-        | jq -r ".balances[] | select(.denom == \"$denom\") | .amount")
+    prev_balance=$(get_balance "$test_addr_1")
 
     gaiad tx bank send \
         "$multisig_addr_1" \
@@ -37,18 +41,19 @@ wait_till_next_block() {
         --chain-id=testhub \
         --generate-only > unsignedTx.json
 
+
     cd "$HOME/multisig/tests/user1"
     multisig tx push "$HOME/unsignedTx.json" cosmos test
     multisig sign cosmos test --from test_key_1
 
     cd "$HOME/multisig/tests/user2"
     multisig sign cosmos test --from test_key_2
+
     multisig broadcast cosmos test
 
     wait_till_next_block
 
-    new_balance=$(gaiad query bank balances "$test_addr_1" --output json  \
-        | jq -r ".balances[] | select(.denom == \"$denom\") | .amount")
+    new_balance=$(get_balance "$test_addr_1")
 
     assert bash -c "(( $new_balance > $prev_balance ))"
 }
