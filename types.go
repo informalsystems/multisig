5fb427bac11ac31edca94afcb66bcf2241654956
Andy Nogueira <me@andynogueira.dev>
2022-08-26 14:35:47 -0400

    Support to sign evmos (#54)

--- types.go
+++ types.go
@@ -73,6 +73,24 @@ type PeriodicVestingAccount struct {
 	} `json:"vesting_periods"`
 }
 
+type EthAccount struct {
+	Type        string `json:"@type"`
+	BaseAccount struct {
+		Address string `json:"address"`
+		PubKey  struct {
+			Type       string `json:"@type"`
+			Threshold  int    `json:"threshold"`
+			PublicKeys []struct {
+				Type string `json:"@type"`
+				Key  string `json:"key"`
+			} `json:"public_keys"`
+		} `json:"pub_key"`
+		AccountNumber string `json:"account_number"`
+		Sequence      string `json:"sequence"`
+	} `json:"base_account"`
+	CodeHash string `json:"code_hash"`
+}
+
 type BaseAccount struct {
 	Type    string `json:"@type"`
 	Address string `json:"address"`
